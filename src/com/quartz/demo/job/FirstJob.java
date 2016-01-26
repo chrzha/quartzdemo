13efa7908342ae1edb1a61f8b7c735b6b4095a90
chrzha <1032537895@qq.com>
2016-01-26 22:59:11 +0800

    jobstore

--- src/com/quartz/demo/job/FirstJob.java
+++ src/com/quartz/demo/job/FirstJob.java
@@ -1,9 +1,14 @@
 package com.quartz.demo.job;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;  
+
 import org.quartz.Job;  
+import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;  
 import org.quartz.JobExecutionException;  
+import org.quartz.JobKey;
+import org.quartz.SchedulerException;
   
 public class FirstJob implements Job {  
   
@@ -14,8 +19,21 @@ public class FirstJob implements Job {
      
     public void execute(JobExecutionContext context)  
         throws JobExecutionException {  
+    	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+
+            JobDataMap dataMap = context.getJobDetail().getJobDataMap();
+
+            String name = dataMap.getString("name");
+        	System.out.println(Thread.currentThread().getName()+" started at: \t" + sdf.format(new Date()));  
+			Thread.currentThread().sleep(10000L);
+			System.out.println("hello," + name);
+			System.out.println(Thread.currentThread().getName() + " finished at: \t" + sdf.format(new Date()));  
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
           
-        System.out.println(" FirstJob: " + new Date());  
-          
+        System.out.println("---------------------------------------"); 
     }  
 } 
--- src/com/quartz/demo/job/SimpleExample.java
+++ src/com/quartz/demo/job/SimpleExample.java
@@ -1,5 +1,7 @@
 package com.quartz.demo.job;
 
+import java.util.Properties;
+
 import org.quartz.CronScheduleBuilder;
 import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
@@ -13,15 +15,15 @@ public class SimpleExample {
     public void run() throws Exception {  
     	SchedulerFactory schedulerFactory = new StdSchedulerFactory();
         Scheduler scheduler = schedulerFactory.getScheduler();
- 
+        scheduler.clear();
         JobDetail jobDetail = JobBuilder.newJob()
                 .ofType(FirstJob.class)
-                .usingJobData("Test1","Quartz")
+                .usingJobData("name","chris")
                 .withIdentity("Test1","Group1")
                 .build();//通过JobBuilder构建JobDetailImpl 实例,也可以直接new JobDetailImpl
  
         Trigger trigger = TriggerBuilder.newTrigger()
-                .withSchedule(CronScheduleBuilder.cronSchedule("0/3 * * * * ?"))
+                .withSchedule(CronScheduleBuilder.cronSchedule("0/3 * * * * ?").withMisfireHandlingInstructionFireAndProceed())
                 .forJob("Test1","Group1").startNow()      //Trigger找到对应的名称为Test1组为Group1的Job,如果不存在则会在执行scheduler.scheduleJob(jobDetail,trigger);报错
                 .build();//通过TriggerBuilder构建CronTriggerImpl实例,也可以直接new CronTriggerImpl
  
